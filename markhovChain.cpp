c1e36fd4809bbf2a339dca86e6feb9eb8de84742
weston <micrometz@gmail.com>
2019-12-06 15:14:03 -0700

    updates

--- markhovChain.cpp
+++ markhovChain.cpp
@@ -8,64 +8,64 @@ int arry[15][15] = {0};
 
 arry[0][0]=50;
 arry[0][1]=50;
+arry[2][2]=12;
+arry[2][1]=44;
+arry[2][3]=44;
+arry[3][3]=12;
+arry[3][2]=44;
+arry[3][4]=44;
+arry[4][2]=12;
+arry[4][5]=44;
+arry[4][3]=44;
+arry[5][5]=12;
+arry[5][4]=44;
+arry[5][6]=44;
+arry[6][6]=12;
+arry[6][7]=44;
+arry[6][5]=44;
+arry[7][7]=12;
+arry[7][8]=44;
+arry[7][6]=44;
+arry[8][8]=12;
+arry[8][9]=44;
+arry[8][7]=44;
+arry[9][9]=12;
+arry[9][10]=44;
+arry[9][8]=44;
+arry[10][10]=12;
+arry[10][9]=44;
+arry[10][11]=44;
+arry[11][11]=12;
+arry[11][12]=44;
+arry[11][10]=44;
+arry[12][12]=12;
+arry[12][13]=44;
+arry[12][11]=44;
+arry[13][13]=12;
+arry[13][14]=44;
+arry[13][12]=44;
+arry[14][14]=12;
+arry[14][15]=44;
+arry[14][13]=44;
 
-arry[2][1]=50;
-arry[2][3]=50;
-
-arry[3][2]=50;
-arry[3][4]=50;
-
-arry[4][5]=50;
-arry[4][3]=50;
-
-arry[5][4]=50;
-arry[5][6]=50;
-
-arry[6][7]=50;
-arry[6][5]=50;
-
-arry[7][8]=50;
-arry[7][6]=50;
-
-arry[8][9]=50;
-arry[8][7]=50;
-/*
-arry[9][10]=50;
-arry[9][8]=50;
-
-arry[10][9]=50;
-arry[10][11]=50;
-
-arry[11][12]=50;
-arry[11][10]=50;
-
-arry[12][13]=50;
-arry[12][11]=50;
-
-arry[13][14]=50;
-arry[13][12]=50;
-
-arry[14][15]=50;
-arry[14][13]=50;
-*/
 srand(time(NULL));
 int node=0;
-int test = 100;//how many times we run it
+int test = 1000;//number of times we test
 int total[test]= {0};
 size_t s = 0; // numeber of step
-int d = rand() % 100; //random gen 
+int randN = srand() % 100; //random gen 
 for(int i = 0; i < test; i++)
 {
-	while(node != 10)
+	while(node != 15)
 	{
 		int totalP = 0;
-		d = rand() % 100;
-		for(int n=0;n<10;n++)
+		randN = srand() % 100;
+		for(int j=0;j<15;j++)
 		{
-			totalP = totalP + arry[node][n];
-			if(d<totalP)
+			totalP = totalP + arry[node][j];
+			if(randN<totalP)
 			{
-				node = n;
+				node = j;
 				s++;
 				break;
 			}
@@ -95,9 +95,9 @@ for (int i = 0; i < test; i++)
 }
 int meanTotal = (mean/test); 
 
+std::cout << "Mean: " << meanTotal << std::endl;
 std::cout << "Max: " << maxTotal << std::endl;
 std::cout << "Min: " << minTotal << std::endl;
-std::cout << "Mean: " << meanTotal << std::endl;
 
 return 0;
 }
